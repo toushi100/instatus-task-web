93f535d3386a04500c946c838663daafffcac78d
Ahmed Salah <toushi2009@gmail.com>
2024-07-02 21:33:14 +0300

    feat: Add SearchBar component

---
+++ src/app/components/search-bar/index.tsx
@@ -0,0 +1,11 @@
+export function SearchBar() {
+  return (
+    <div className="px-6 pt-8 pb-2 table-head-color">
+      <input
+        className="search-bar-input w-full pl-1"
+        type="text"
+        placeholder="Search name, email or action..."
+      />
+    </div>
+  );
+}
