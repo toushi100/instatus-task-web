df08b18f290c13402db66f5494bc8ca94eae5ecf
Ahmed Salah <toushi2009@gmail.com>
2024-07-05 20:06:21 +0300

    refactor: Update search bar styling to include border-radius

--- src/app/components/search-bar/index.tsx
+++ src/app/components/search-bar/index.tsx
@@ -1,6 +1,6 @@
 export function SearchBar() {
   return (
-    <div className="px-6 pt-8 pb-2 table-head-color">
+    <div className="px-6 pt-8 pb-2 table-head-color border-radius">
       <input
         className="search-bar-input w-full pl-1"
         type="text"
