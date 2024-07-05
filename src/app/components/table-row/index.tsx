4b5ed1420458275fa0ba4bc6d56fa0bf15786a8e
Ahmed Salah <toushi2009@gmail.com>
2024-07-05 20:06:38 +0300

    refactor: Update TableRow component to include event data and visibility prop

--- src/app/components/table-row/index.tsx
+++ src/app/components/table-row/index.tsx
@@ -1,24 +1,66 @@
 import React from "react";
 import Image from "next/image";
 import arrowRight from "../../public/vector-stroke.svg";
-export function TableRow() {
+import { EventData } from "../detail";
+export function TableRow({
+  eventData,
+  visibility,
+}: {
+  eventData: EventData;
+
+  visibility: number | null;
+}) {
+  const showDetail = visibility === eventData.id ? "hidden" : "block";
   return (
-    <tbody>
-      <tr className="">
-        <th scope="row" className="px-6 py-4  regular">
-          Apple MacBook Pro 17"
-        </th>
-        <td className="px-6 py-4 regular">Silver</td>
-        <td className="px-6 py-4 regular">Laptop</td>
-        <td className="justify-items-end">
-          <Image
-            src="/vector-stroke.svg"
-            alt="arrow-right"
-            width={9}
-            height={14}
-          />
-        </td>
-      </tr>
-    </tbody>
+    <div className={`${showDetail}`}>
+      <div className="container mx-auto py-3 px-6  border">
+        <div className="flex flex-wrap">
+          {/* <div className="w-full md:w-1/3 px-6">
+            <div className=" detail-headers py-3">{eventData.group}</div>
+          </div> */}
+          <div className="w-full md:w-1/3 px-6 flex items-center">
+            <div className="ml-2 image-sub">
+              <div className="single-letter ">
+                {eventData.actor_name.charAt(0).toUpperCase()}
+              </div>
+            </div>
+            <div className="detail-headers py-3 mx-4">{eventData.group}</div>
+          </div>
+          <div className="w-full md:w-1/3 px-6">
+            <div className="detail-headers py-3">{eventData.action_name}</div>
+          </div>
+
+          {/* <div className="w-full md:w-1/3  px-6 ">
+            <div className="detail-headers py-3">
+              {eventData.occurred_at.toString()}
+            </div>
+            <div className="ml-2">
+              <Image
+                className="bg-red-500  justify-items-end"
+                src="/vector-stroke.svg"
+                alt="arrow-right"
+                width={9}
+                height={14}
+              />
+            </div>
+          </div> */}
+
+          <div className="w-full md:w-1/3 px-6  flex items-center">
+            <div className="detail-headers py-3">
+              {eventData.occurred_at.toString()}
+            </div>
+            <div className="ml-2 w-full flex  ">
+              <Image
+                className=" ml-auto "
+                src="/vector-stroke.svg"
+                alt="arrow-right"
+                width={9}
+                height={14}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
