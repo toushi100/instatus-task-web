25e7a8b173bd86f4259229aec5151029b47e84b5
Ahmed Salah <toushi2009@gmail.com>
2024-07-05 20:05:58 +0300

    feat: Add events module for managing events

---
+++ tools/endpoints/events.ts
@@ -0,0 +1,42 @@
+import client from "../axios/api";
+
+type ApiCommonParams = {
+  [key: string]: any;
+};
+
+type CreateEventParams = {
+  actor_name: String;
+  group: String;
+  action_name: String;
+  target_name: String;
+  location: String;
+  metadata_redirect: String;
+  metadata_description: String;
+};
+
+export function index(commonApiParams?: ApiCommonParams) {
+  const params = commonApiParams || {};
+
+  return client.get("", { params: params });
+}
+
+export function create(createEventParams: CreateEventParams) {
+  const {
+    actor_name,
+    group,
+    action_name,
+    target_name,
+    location,
+    metadata_redirect,
+    metadata_description,
+  } = createEventParams;
+  return client.post("", {
+    actor_name,
+    group,
+    action_name,
+    target_name,
+    location,
+    metadata_redirect,
+    metadata_description,
+  });
+}
