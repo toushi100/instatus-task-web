8ae21a6043b55ffbf046d21ab1f2ea2de588b8b2
Ahmed Salah <toushi2009@gmail.com>
2024-07-05 21:57:47 +0300

    refactor: Update Table component to include lazy loading for images

--- src/app/components/table/index.tsx
+++ src/app/components/table/index.tsx
@@ -4,41 +4,41 @@ import { Detail, EventData } from "../detail";
 import { SearchBar } from "../search-bar";
 import { TableRow } from "../table-row";
 import { index } from "../../../../tools/endpoints/events";
+
 export function Table() {
   const [eventData, setEventData] = useState<EventData[]>([]);
+  const [count, setCount] = useState(0);
+  const [skip, setSkip] = useState(0);
   const [expandedRow, setExpandedRow] = useState<number | null>(null);
+
+  const take = 10;
+  const paginatianParams = {
+    skip,
+    take,
+  };
+  const getNextBatch = () => {
+    if (count < take) return;
+    setSkip(skip + take + 1);
+  };
   useEffect(() => {
     const fetchData = async () => {
-      const response = await index();
-      setEventData(response.data);
+      const response = await index(paginatianParams);
+      setEventData([...eventData, ...response.data]);
+      setCount(response.data.length);
     };
     fetchData();
-  }, []);
+  }, [skip]);
 
   const handleRowClick = (id: number) => {
     setExpandedRow(expandedRow === id ? null : id);
   };
-  // const expandedRow = null;
+  const disableLoadMore = count < take ? "disabled" : "";
   return (
     <>
       <div className=" w-10/12 ">
         <div className="mx-auto">
           <SearchBar />
-          {/* <table className="w-full text-left  ">
-            <thead className="table-head-color block">
-              <tr>
-                <th scope="col" className="px-6 py-3 headers w-1/3">
-                  ACTOR
-                </th>
-                <th scope="col" className="px-6 py-3 headers w-1/3">
-                  ACTION
-                </th>
-                <th scope="col" className="px-6 py-3 headers w-1/3">
-                  DATE
-                </th>
-                <th scope="col" className="px-6 py-3 "></th>
-              </tr>
-            </thead> */}
+
           <div className="container mx-auto py-6 px-8 table-head-color ">
             <div className="flex flex-wrap">
               <div className="w-full md:w-1/3 px-6">
@@ -59,8 +59,13 @@ export function Table() {
               <Detail eventData={event} visibility={expandedRow} />
             </div>
           ))}
-          {/* </table> */}
-          <button className="w-full py-5 load-more-color">Load More</button>
+
+          <button
+            className={`w-full py-5 load-more-color ${disableLoadMore}`}
+            onClick={() => getNextBatch()}
+          >
+            LOAD MORE
+          </button>
         </div>
       </div>
     </>
