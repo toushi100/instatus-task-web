f592debbd4cc1b30cf62d53bd6ea4b4a623efd6b
Ahmed Salah <toushi2009@gmail.com>
2024-07-06 19:28:03 +0300

    feat: add interval polling for live updates

--- src/app/components/table/index.tsx
+++ src/app/components/table/index.tsx
@@ -4,7 +4,7 @@ import { Detail, EventData } from "../detail";
 import { SearchBar } from "../search-bar";
 import { TableRow } from "../table-row";
 import { index } from "../../../../tools/endpoints/events";
-import useSWR from "swr";
+import { all } from "axios";
 
 export function Table() {
   const [eventData, setEventData] = useState<EventData[]>([]);
@@ -12,26 +12,25 @@ export function Table() {
   const [skip, setSkip] = useState(0);
   const [search, setSearch] = useState("");
   const [live, setLive] = useState(false);
+
   const [expandedRow, setExpandedRow] = useState<number | null>(null);
 
   const take = 10;
+
   const params = {
     skip,
     take,
     q: search,
   };
-  const { data, error, isLoading } = useSWR("/", index(params), {
-    refreshInterval: 1000,
-  });
-
   const getNextBatch = () => {
     if (count < take) return;
     setSkip(skip + take + 1);
+    setLive(false);
   };
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await index(params);
+      let response = await index(params);
       if (params.q) {
         setEventData(response.data);
       } else {
@@ -42,6 +41,25 @@ export function Table() {
     fetchData();
   }, [skip, search]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      let response = await index({ skip: 0, take: eventData.length });
+      setEventData(response.data);
+    };
+    let interval = null;
+    if (live) {
+      interval = setInterval(() => {
+        fetchData();
+      }, 1000 * 60 * 5); // 5 mins
+    } else if (interval) {
+      clearInterval(interval);
+    }
+    return () => {
+      if (interval) {
+        clearInterval(interval);
+      }
+    };
+  }, [live]);
   const handleSearchBarData = (input: string) => {
     setSearch(input);
   };
@@ -51,7 +69,7 @@ export function Table() {
   const handleRowClick = (id: number) => {
     setExpandedRow(expandedRow === id ? null : id);
   };
-  const disableLoadMore = count < take ? "disabled" : "";
+  const disableLoadMore = count < take ? "disabled cursor-not-allowed" : "";
 
   return (
     <>
@@ -73,7 +91,7 @@ export function Table() {
               </div>
 
               <div className="w-full md:w-1/3 px-6">
-                <div className="detail-headers py-3">DATE</div>
+                <div className="detail-headers py-3 ">DATE</div>
               </div>
             </div>
           </div>
