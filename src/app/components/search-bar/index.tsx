a81360943cfaab9441d8cbfb27d849fc247bd738
Ahmed Salah <toushi2009@gmail.com>
2024-07-06 12:11:56 +0300

    refactor: Update SearchBar component to include onChange event handler

--- src/app/components/search-bar/index.tsx
+++ src/app/components/search-bar/index.tsx
@@ -1,10 +1,17 @@
-export function SearchBar() {
+"use client";
+import React, { useState } from "react";
+export function SearchBar({ onData }: any) {
+  const handleInputChange = (event: any) => {
+    onData(event.target.value);
+  };
+
   return (
     <div className="px-6 pt-8 pb-2 table-head-color border-radius">
       <input
         className="search-bar-input w-full pl-1"
         type="text"
         placeholder="Search name, email or action..."
+        onChange={handleInputChange}
       />
     </div>
   );
