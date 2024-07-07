2f5c5f4780222b45e19f6d3c92688cb1571ea8e6
Ahmed Salah <toushi2009@gmail.com>
2024-07-07 14:40:53 +0300

    refactor: Add export functionality to SearchBar component

--- src/app/components/search-bar/index.tsx
+++ src/app/components/search-bar/index.tsx
@@ -1,6 +1,17 @@
 "use client";
 import React, { useState } from "react";
-export function SearchBar({ onData, onLive, currentLiveValue }: any) {
+import Image from "next/image";
+
+export function SearchBar({
+  onData,
+  onLive,
+  currentLiveValue,
+  onExport,
+  exportButtonClicked,
+}: any) {
+  const handleExport = () => {
+    onExport(!exportButtonClicked);
+  };
   const handleInputChange = (event: any) => {
     onData(event.target.value);
   };
@@ -8,14 +19,32 @@ export function SearchBar({ onData, onLive, currentLiveValue }: any) {
     onLive(!currentLiveValue);
   };
   const liveButtonText = currentLiveValue ? "PAUSE" : "LIVE";
+  const exportButtonClickedText = exportButtonClicked ? "CANCEL" : "EXPORT";
   return (
     <div className="px-6 pt-8 pb-2 table-head-color w-full flex items-center border-radius">
       <input
-        className="search-bar-input w-full"
+        className="search-bar-input w-full "
         type="text"
-        placeholder="Search name, email or action..."
+        placeholder=" Search name, email or action..."
         onChange={handleInputChange}
       />
+      <div className="flex">
+        <button
+          className=" ml-auto flex items-center export-button  px-4"
+          onClick={handleExport}
+        >
+          <div className=" mr-1">
+            <Image
+              className=" mx-auto pb-1"
+              src="/export-vector.svg"
+              alt="arrow-right"
+              width={49}
+              height={0}
+            />
+          </div>
+          {exportButtonClickedText}
+        </button>
+      </div>
       <div className="flex">
         <button
           className=" ml-auto flex items-center live-button  px-4"
